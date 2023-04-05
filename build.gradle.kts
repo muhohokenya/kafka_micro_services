3012472b4c08bf942cb7dd62ef64c42f2458cc72
muhohoweb <muhohokenya@users.noreply.github.com>
2023-04-05 13:28:20 +0300

    Jenkin File

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,13 +16,36 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.springframework.kafka:spring-kafka")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("com.mysql:mysql-connector-j")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.springframework.kafka:spring-kafka-test")
+    testImplementation("org.springframework.security:spring-security-test")
+
+    //JWT
+    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    //kafka
+    implementation("org.springframework.kafka:spring-kafka")
+
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.14.2")
+    implementation("com.launchdarkly:okhttp-eventsource:4.1.0")
+
+
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
+
+    implementation("com.fasterxml.jackson.core:jackson-core:2.14.2")
 }
 
 tasks.withType<KotlinCompile> {
